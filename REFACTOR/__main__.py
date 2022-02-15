1115718395e0d1d1fda28423156bbbe78a94dc6f
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-02-14 23:21:45 -0500

    Init GUI
    Opens basic GUI, as well as adding in functions into gui object

--- REFACTOR/__main__.py
+++ REFACTOR/__main__.py
@@ -2,23 +2,52 @@ import sys
 import os
 from tkinter import *
 from PIL import Image, ImageTk
+import data
+import workoutInit
 
 
 class Gui:
     def __init__(self):
-        self.screen = Tk()
-        self.screen.title("Super-Set: The App")
+
+        # Window Config
+        self.window = Tk()
+        self.window.title("Super-Set: The App")
         self.screenWidth = 800
         self.screenHeight = 800
-        self.screen.geometry(f"{self.screenWidth}x{self.screenHeight}")
+        self.window.geometry(f"{self.screenWidth}x{self.screenHeight}")
 
-        self.window = Label(self.screen, image="")
-        self.window.pack()
+        # Button Init
+        self.intro = Label(self.window, bg="white", text="")
+        self.intro.grid(row=0, column=3, columnspan=2, sticky="n")
+
+        button_yes = Button(gui, text="Workout", width=10,
+                            command=lambda: self.workout(self.window, self.incompleteWorkouts))
+        button_yes.grid(row=8, column=1)
+
+        button_no = Button(gui, text="Exit", width=10, command=quit)
+        button_no.grid(row=8, column=7)
+
+        button_save = Button(gui, text="Save", width=10, command=lambda: data.saveProgress(self.window, Workout.completeWorkouts))
+        button_save.grid(row=9, column=1)
+
+        button_load = Button(gui, text="Load", width=10, command=lambda: data.loadProgress(self.intro))
+        button_load.grid(row=9, column=7)
+
+        button_progress = Button(gui, text="Progress", width=10, command=lambda: data.progress(intro))
+        button_progress.grid(row=8, column=3, rowspan=2, columnspan=2, padx=100)
+
+        numSetsReps_Label = Label(gui, bg="white", text="Sets / Reps")
+        numSetsReps_Label.grid(row=3, column=1, columnspan=2, sticky="new")
+
+        numSetsReps_Box = Label(gui, width="20", bg="black", fg="white")
+        numSetsReps_Box.grid(row=3, column=2, columnspan=2, sticky="new")
+        # self.screen = Label(self.screen, image="")
+        # self.screen.pack()
 
 
 if __name__ == "__main__":
     gui = Gui()
-
-    gui.screen.mainloop()
+    workoutInit.programInit()
+    gui.window.mainloop()
 
 sys.exit()
--- REFACTOR/data.py
+++ REFACTOR/data.py
@@ -1,27 +1,28 @@
+import __main__
 
 
 # CLASS FOR WORKOUT DATA
 def saveProgress(dialog, complete):
     # Removes boxes when out of workout function
-    numSetsReps_Box.grid_remove()
-    numSetsReps_Label.grid_remove()
+    __main__.gui.numSetsReps_Box.grid_remove()
+    __main__.gui.numSetsReps_Label.grid_remove()
     img.grid_remove()
 
     saveData = open("resources/data/.workoutPicker_SAVE.tsv", "w+", newline="\n")
     # Saves completed workouts to file
     for i in complete:
         saveData.writelines(i + "\n")
-    button_yes["text"] = "Yes"
-    button_no["text"] = "No"
+    __main__.gui.button_yes["text"] = "Yes"
+    __main__.gui.button_no["text"] = "No"
     dialog["text"] = "Progress saved! Would you like to continue your workout?"
-    button_yes["command"] = lambda: Workout.workout(intro, Workout.incompleteWorkouts)
-    button_no["command"] = quit
+    __main__.gui.button_yes["command"] = lambda: Workout.workout(intro, Workout.incompleteWorkouts)
+    __main__.gui.button_no["command"] = quit
 
 
 def loadProgress(dialog):
     # Removes boxes when out of workout function
-    numSetsReps_Box.grid_remove()
-    numSetsReps_Label.grid_remove()
+    __main__.gui.numSetsReps_Box.grid_remove()
+    __main__.numSetsReps_Label.grid_remove()
     img.grid_remove()
 
     Workout.incompleteWorkouts = Workout.newFile()
@@ -40,3 +41,27 @@ def loadProgress(dialog):
         for y in Workout.incompleteWorkouts:
             if x == y:
                 Workout.incompleteWorkouts.remove(y)
+        # Displays complete/incomplete workouts (displays new row of information)
+        __main__.gui.currentWorkout_Label["text"] = "Complete Workouts: "
+        __main__.gui.currentWorkout_Box["text"] = Workout.completeWorkouts
+        __main__.gui.incompleteWorkout_Label.grid(row=3, column=0, columnspan=2, sticky="new")
+        __main__.gui.incompleteWorkout_Box.grid(row=3, column=2, columnspan=2, sticky="new")
+        __main__.gui.incompleteWorkout_Box["text"] = Workout.incompleteWorkouts
+
+
+def progress(dialog):
+    # Removes boxes when out of workout function
+    __main__.gui.numSetsReps_Box.grid_remove()
+    __main__.gui.numSetsReps_Label.grid_remove()
+    img.grid_remove()
+
+    dialog["text"] = "Progress Loaded..."
+    __main__.gui.currentWorkout_Label["text"] = "Complete Workouts: "
+    __main__.gui.currentWorkout_Box["text"] = Workout.completeWorkouts
+    __main__.gui.incompleteWorkout_Label.grid(row=3, column=0, columnspan=2, sticky="new")
+    __main__.gui.incompleteWorkout_Box.grid(row=3, column=2, columnspan=2, sticky="new")
+    __main__.gui.incompleteWorkout_Box["text"] = Workout.incompleteWorkouts
+
+    # checks if list of incomplete workouts is empty
+    if len(Workout.incompleteWorkouts) == 0:
+        dialog["text"] = "You have completed all your workouts!"
