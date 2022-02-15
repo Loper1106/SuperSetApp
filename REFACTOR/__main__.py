68f70de52e85e83b84f948eea34e69077c534027
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-02-14 23:23:01 -0500

    Update __main__.py
    Changing"gui" to "self.window" for functionability.

--- REFACTOR/__main__.py
+++ REFACTOR/__main__.py
@@ -20,26 +20,26 @@ class Gui:
         self.intro = Label(self.window, bg="white", text="")
         self.intro.grid(row=0, column=3, columnspan=2, sticky="n")
 
-        button_yes = Button(gui, text="Workout", width=10,
+        button_yes = Button(self.window, text="Workout", width=10,
                             command=lambda: self.workout(self.window, self.incompleteWorkouts))
         button_yes.grid(row=8, column=1)
 
-        button_no = Button(gui, text="Exit", width=10, command=quit)
+        button_no = Button(self.window, text="Exit", width=10, command=quit)
         button_no.grid(row=8, column=7)
 
-        button_save = Button(gui, text="Save", width=10, command=lambda: data.saveProgress(self.window, Workout.completeWorkouts))
+        button_save = Button(self.window, text="Save", width=10, command=lambda: data.saveProgress(self.window, Workout.completeWorkouts))
         button_save.grid(row=9, column=1)
 
-        button_load = Button(gui, text="Load", width=10, command=lambda: data.loadProgress(self.intro))
+        button_load = Button(self.window, text="Load", width=10, command=lambda: data.loadProgress(self.intro))
         button_load.grid(row=9, column=7)
 
-        button_progress = Button(gui, text="Progress", width=10, command=lambda: data.progress(intro))
+        button_progress = Button(self.window, text="Progress", width=10, command=lambda: data.progress(intro))
         button_progress.grid(row=8, column=3, rowspan=2, columnspan=2, padx=100)
 
-        numSetsReps_Label = Label(gui, bg="white", text="Sets / Reps")
+        numSetsReps_Label = Label(self.window, bg="white", text="Sets / Reps")
         numSetsReps_Label.grid(row=3, column=1, columnspan=2, sticky="new")
 
-        numSetsReps_Box = Label(gui, width="20", bg="black", fg="white")
+        numSetsReps_Box = Label(self.window, width="20", bg="black", fg="white")
         numSetsReps_Box.grid(row=3, column=2, columnspan=2, sticky="new")
         # self.screen = Label(self.screen, image="")
         # self.screen.pack()
