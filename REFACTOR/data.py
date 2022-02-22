c66727881a8575a387f75872e630d0dc1201c284
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-02-21 22:02:10 -0500

    Update data.py
    Changing directory for refactor

--- REFACTOR/data.py
+++ REFACTOR/data.py
@@ -1,28 +1,28 @@
-import __main__
+import __main__, screenDisplay
 
 
 # CLASS FOR WORKOUT DATA
 def saveProgress(dialog, complete):
     # Removes boxes when out of workout function
-    __main__.gui.numSetsReps_Box.grid_remove()
-    __main__.gui.numSetsReps_Label.grid_remove()
+    screenDisplay.gui.numSetsReps_Box.grid_remove()
+    screenDisplay.gui.numSetsReps_Label.grid_remove()
     img.grid_remove()
 
     saveData = open("resources/data/.workoutPicker_SAVE.tsv", "w+", newline="\n")
     # Saves completed workouts to file
     for i in complete:
         saveData.writelines(i + "\n")
-    __main__.gui.button_yes["text"] = "Yes"
-    __main__.gui.button_no["text"] = "No"
+    screenDisplay.gui.button_yes["text"] = "Yes"
+    screenDisplay.gui.button_no["text"] = "No"
     dialog["text"] = "Progress saved! Would you like to continue your workout?"
-    __main__.gui.button_yes["command"] = lambda: Workout.workout(intro, Workout.incompleteWorkouts)
-    __main__.gui.button_no["command"] = quit
+    screenDisplay.gui.button_yes["command"] = lambda: Workout.workout(intro, Workout.incompleteWorkouts)
+    screenDisplay.gui.button_no["command"] = quit
 
 
 def loadProgress(dialog):
     # Removes boxes when out of workout function
-    __main__.gui.numSetsReps_Box.grid_remove()
-    __main__.numSetsReps_Label.grid_remove()
+    screenDisplay.gui.numSetsReps_Box.grid_remove()
+    screenDisplay.gui.numSetsReps_Label.grid_remove()
     img.grid_remove()
 
     Workout.incompleteWorkouts = Workout.newFile()
@@ -42,25 +42,25 @@ def loadProgress(dialog):
             if x == y:
                 Workout.incompleteWorkouts.remove(y)
         # Displays complete/incomplete workouts (displays new row of information)
-        __main__.gui.currentWorkout_Label["text"] = "Complete Workouts: "
-        __main__.gui.currentWorkout_Box["text"] = Workout.completeWorkouts
-        __main__.gui.incompleteWorkout_Label.grid(row=3, column=0, columnspan=2, sticky="new")
-        __main__.gui.incompleteWorkout_Box.grid(row=3, column=2, columnspan=2, sticky="new")
-        __main__.gui.incompleteWorkout_Box["text"] = Workout.incompleteWorkouts
+        screenDisplay.gui.currentWorkout_Label["text"] = "Complete Workouts: "
+        screenDisplay.gui.currentWorkout_Box["text"] = Workout.completeWorkouts
+        screenDisplay.gui.incompleteWorkout_Label.grid(row=3, column=0, columnspan=2, sticky="new")
+        screenDisplay.gui.incompleteWorkout_Box.grid(row=3, column=2, columnspan=2, sticky="new")
+        screenDisplay.gui.incompleteWorkout_Box["text"] = Workout.incompleteWorkouts
 
 
 def progress(dialog):
     # Removes boxes when out of workout function
-    __main__.gui.numSetsReps_Box.grid_remove()
-    __main__.gui.numSetsReps_Label.grid_remove()
+    screenDisplay.gui.numSetsReps_Box.grid_remove()
+    screenDisplay.gui.numSetsReps_Label.grid_remove()
     img.grid_remove()
 
     dialog["text"] = "Progress Loaded..."
-    __main__.gui.currentWorkout_Label["text"] = "Complete Workouts: "
-    __main__.gui.currentWorkout_Box["text"] = Workout.completeWorkouts
-    __main__.gui.incompleteWorkout_Label.grid(row=3, column=0, columnspan=2, sticky="new")
-    __main__.gui.incompleteWorkout_Box.grid(row=3, column=2, columnspan=2, sticky="new")
-    __main__.gui.incompleteWorkout_Box["text"] = Workout.incompleteWorkouts
+    screenDisplay.gui.currentWorkout_Label["text"] = "Complete Workouts: "
+    screenDisplay.gui.currentWorkout_Box["text"] = Workout.completeWorkouts
+    screenDisplay.gui.incompleteWorkout_Label.grid(row=3, column=0, columnspan=2, sticky="new")
+    screenDisplay.gui.incompleteWorkout_Box.grid(row=3, column=2, columnspan=2, sticky="new")
+    screenDisplay.gui.incompleteWorkout_Box["text"] = Workout.incompleteWorkouts
 
     # checks if list of incomplete workouts is empty
     if len(Workout.incompleteWorkouts) == 0:
