0140134cdd2b97a1ba246dbaf1b68f2d7c90b258
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-02-21 22:02:34 -0500

    Update __main__.py
    Moving display class out of Main

--- REFACTOR/__main__.py
+++ REFACTOR/__main__.py
@@ -4,50 +4,10 @@ from tkinter import *
 from PIL import Image, ImageTk
 import data
 import workoutInit
-
-
-class Gui:
-    def __init__(self):
-
-        # Window Config
-        self.window = Tk()
-        self.window.title("Super-Set: The App")
-        self.screenWidth = 800
-        self.screenHeight = 800
-        self.window.geometry(f"{self.screenWidth}x{self.screenHeight}")
-
-        # Button Init
-        self.intro = Label(self.window, bg="white", text="")
-        self.intro.grid(row=0, column=3, columnspan=2, sticky="n")
-
-        button_yes = Button(self.window, text="Workout", width=10,
-                            command=lambda: self.workout(self.window, self.incompleteWorkouts))
-        button_yes.grid(row=8, column=1)
-
-        button_no = Button(self.window, text="Exit", width=10, command=quit)
-        button_no.grid(row=8, column=7)
-
-        button_save = Button(self.window, text="Save", width=10, command=lambda: data.saveProgress(self.window, Workout.completeWorkouts))
-        button_save.grid(row=9, column=1)
-
-        button_load = Button(self.window, text="Load", width=10, command=lambda: data.loadProgress(self.intro))
-        button_load.grid(row=9, column=7)
-
-        button_progress = Button(self.window, text="Progress", width=10, command=lambda: data.progress(intro))
-        button_progress.grid(row=8, column=3, rowspan=2, columnspan=2, padx=100)
-
-        numSetsReps_Label = Label(self.window, bg="white", text="Sets / Reps")
-        numSetsReps_Label.grid(row=3, column=1, columnspan=2, sticky="new")
-
-        numSetsReps_Box = Label(self.window, width="20", bg="black", fg="white")
-        numSetsReps_Box.grid(row=3, column=2, columnspan=2, sticky="new")
-        # self.screen = Label(self.screen, image="")
-        # self.screen.pack()
+import screenDisplay
 
 
 if __name__ == "__main__":
-    gui = Gui()
-    workoutInit.programInit()
-    gui.window.mainloop()
+    screenDisplay.runHub()
 
 sys.exit()
