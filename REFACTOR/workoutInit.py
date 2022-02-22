6f59ffe343d1338a29a342576f2c809ade318b06
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-02-22 01:37:09 -0500

    Update workoutInit.py
    Create global class for workout lists. Added debug function to test reading lists.

--- REFACTOR/workoutInit.py
+++ REFACTOR/workoutInit.py
@@ -1,8 +1,28 @@
 import __main__
-import screenDisplay
+import screenDisplay, data
+
+
+class WorkoutLists:
+
+    def __init__(self):
+        self.allWorkouts = []
+        self.incompleteWorkouts = []
+        self.completeWorkouts = []
 
 
 def programInit():
+
+    dictionary = WorkoutLists()
+    data.importAllWorkouts(dictionary.allWorkouts)
+
+    # DEBUG
+    for i in dictionary.allWorkouts:
+        print("NEW ITERATION")
+        print("Name: ", i.workoutName)
+        print("Description: ",i.workoutDesc)
+        print("File Location: ", i.imgLocation)
+        print()
+
     ##############
     # DIALOG BOX #
     ##############
