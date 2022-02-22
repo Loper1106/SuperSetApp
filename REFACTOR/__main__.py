e0cf6b34c98189fc7f8944fe1758f683c090ab1b
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-02-22 01:38:47 -0500

    Update __main__.py
    Cleaned up unused imports

--- REFACTOR/__main__.py
+++ REFACTOR/__main__.py
@@ -1,11 +1,5 @@
 import sys
-import os
-from tkinter import *
-from PIL import Image, ImageTk
-import data
-import workoutInit
 import screenDisplay
-
 
 if __name__ == "__main__":
     screenDisplay.runHub()
