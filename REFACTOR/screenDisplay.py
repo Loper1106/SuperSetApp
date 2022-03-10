a8e72c812d7198c93fd873864514db9fc0952d7b
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-03-09 19:59:54 -0500

    Update screenDisplay.py
    Revert change

--- REFACTOR/screenDisplay.py
+++ REFACTOR/screenDisplay.py
@@ -14,37 +14,35 @@ class Gui:
         self.screenHeight = 800
         self.window.geometry(f"{self.screenWidth}x{self.screenHeight}")
 
+        # Button Init
+        self.intro = Label(self.window, bg="white", text="WELCOME TO THE SUPER-SET APP!")
+        self.intro.grid(row=0, column=3, columnspan=2, sticky="n")
 
-def buttonInit():
-    # Button Init
-    intro = Label(gui, bg="white", text="WELCOME TO THE SUPER-SET APP!")
-    intro.grid(row=0, column=3, columnspan=2, sticky="n")
+        button_yes = Button(self.window, text="Workout", width=10,
+                            command=lambda: workoutInit.workout(data.WorkoutDictionary.allWorkouts,
+                                                                data.WorkoutDictionary.incompleteWorkouts))
+        button_yes.grid(row=8, column=1)
 
-    button_yes = Button(gui, text="Workout", width=10,
-                        command=lambda: workoutInit.workout(data.WorkoutDictionary.allWorkouts,
-                                                            data.WorkoutDictionary.incompleteWorkouts))
-    button_yes.grid(row=8, column=1)
+        button_no = Button(self.window, text="Exit", width=10, command=quit)
+        button_no.grid(row=8, column=7)
 
-    button_no = Button(gui, text="Exit", width=10, command=quit)
-    button_no.grid(row=8, column=7)
+        button_save = Button(self.window, text="Save", width=10,
+                             command=lambda: data.saveProgress(self.window, data.WorkoutDictionary.completeWorkouts))
+        button_save.grid(row=9, column=1)
 
-    button_save = Button(gui, text="Save", width=10,
-                         command=lambda: data.saveProgress(gui, data.saveProgress()))
-    button_save.grid(row=9, column=1)
+        button_load = Button(self.window, text="Load", width=10, command=lambda: data.loadProgress(self.intro))
+        button_load.grid(row=9, column=7)
 
-    button_load = Button(gui, text="Load", width=10, command=lambda: data.loadProgress())
-    button_load.grid(row=9, column=7)
+        button_progress = Button(self.window, text="Progress", width=10, command=lambda: data.progress(self.intro))
+        button_progress.grid(row=8, column=3, rowspan=2, columnspan=2, padx=100)
 
-    button_progress = Button(gui, text="Progress", width=10, command=lambda: data.progress())
-    button_progress.grid(row=8, column=3, rowspan=2, columnspan=2, padx=100)
+        numSetsReps_Label = Label(self.window, bg="white", text="Sets / Reps")
+        numSetsReps_Label.grid(row=3, column=1, columnspan=2, sticky="new")
 
-    numSetsReps_Label = Label(gui, bg="white", text="Sets / Reps")
-    numSetsReps_Label.grid(row=3, column=1, columnspan=2, sticky="new")
-
-    numSetsReps_Box = Label(gui, width="20", bg="black", fg="white")
-    numSetsReps_Box.grid(row=3, column=2, columnspan=2, sticky="new")
-    # self.screen = Label(self.screen, image="")
-    # self.screen.pack()
+        numSetsReps_Box = Label(self.window, width="20", bg="black", fg="white")
+        numSetsReps_Box.grid(row=3, column=2, columnspan=2, sticky="new")
+        # self.screen = Label(self.screen, image="")
+        # self.screen.pack()
 
 
 def runHub():
@@ -53,7 +51,6 @@ def runHub():
     gui = Gui()
 
     workoutInit.programInit()
-    buttonInit()
 
     gui.window.mainloop()
 
